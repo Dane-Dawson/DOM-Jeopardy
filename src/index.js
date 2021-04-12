1ca45b384ff4b9f49a86a68a920995962eec45ab
Tom Tobar <codetombomb@gmail.com>
2021-04-12 10:25:13 -0500

    add flexbox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
-console.log("running index.js script")
-console.log(jeopardyQuestions)
+// console.log("running index.js script")
+// console.log(jeopardyQuestions)
 
 document.addEventListener("DOMContentLoaded", () => {
 
     jeopardyQuestions.map(question => createTiles(question))
 });
 
-function createTiles(question){
-    console.log(question)
+function createTiles(question) {
+    // console.log(question)
     const board = document.querySelector("#jeopardy-board");
 
-    
-    
+    const tile = document.createElement("div");
+    tile.className = "tile";
+
+    const value = document.createElement("p");
+    value.innerText = question.value;
+
+    value.addEventListener("click", () => {
+        console.log(question)
+        value.remove();
+
+        const prompt = document.createElement("p");
+        prompt.innerText = question.question;
+
+        prompt.addEventListener("click", () => {
+            prompt.remove();
+
+            const answer = document.createElement("p");
+            answer.innerText = question.answer;
+            tile.appendChild(answer);
+
+        })
+
+        tile.appendChild(prompt);
+    })
+    tile.appendChild(value);
+    board.appendChild(tile);
+
 }
+
+
+{/* <div class="cat-col">
+    <div class="tile category"></div>
+    <div class="tile"></div>
+    <div class="tile"></div>
+    <div class="tile"></div>
+    <div class="tile"></div>
+    <div class="tile"></div>
+</div> */}
+
+
+
+
+
+
